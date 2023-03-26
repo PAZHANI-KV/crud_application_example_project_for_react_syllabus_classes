75b65c59dcfa722538ae4e9ba04328202a9a4635
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-03-26 20:54:24 +0530

    changes commited

---
+++ src/core/Base.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function BaseApp({ title, styles, children }) {
+    return (
+        <div>
+            <div>
+                <div className="nav-styles">nav bar</div>
+                <div className={styles}>{title}</div>
+            </div>
+{/* There is a property called children in react.If we call children in the function and declarew it inside a tag, then whatever we are typing in the 
+component inside the tag in App.JS will be set for children it will be displayed.Here children is a keyword like prop*/}
+            <div className="circle" >{children}</div>
+            <footer>
+                Contact Us
+                <div>email: react@email.com</div>
+                <div>phone:98******64 </div>
+            </footer>
+        </div>
+
+    )
+
+}
+
