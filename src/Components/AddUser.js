2cf6675161b6a177d8522c7b73dc21db59ce1022
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-03-27 21:50:51 +0530

    changes commited

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddUser() {
+
+    // defining states for all the inputs
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [batch, setBatch] = useState("")
+    const [email, setEmail] = useState("")
+    const [experience, setExperience] = useState("")
+
+const addNewUser = () => {
+    const newUser = {
+        id,
+        name,
+        batch,
+        email,
+        experience
+    }
+}
+
     return (
         <div>
 
-            <input placeholder="id" />
-            <input placeholder="name" />
-            <input placeholder="batch" />
-            <input placeholder="email" />
-            <input placeholder="experience" />
+
+            <input
+                placeholder="id"
+                value={id}
+                onChange={(event)=> setId(event.target.value)}
+            />
+
+
+            <input
+                placeholder="name" 
+                value={name}
+                onChange={(event)=> setName(event.target.value)}
+            />
+
+
+            <input
+                placeholder="batch"
+                value={batch}
+                onChange={(event)=>setBatch(event.target.value)}
+            />
+
+
+            <input
+                placeholder="email"
+                value={email}
+                onChange={(event)=> setEmail(event.target.value)}
+            />
+
+
+            <input
+                placeholder="experience"
+                value={experience}
+                onChange={(event)=>setExperience(event.target.value)}
+            />
+
+
             <button
-                onClick={() => console.log("clicked")}
+                onClick={addNewUser}
             >Add</button>
+
 
         </div>
     )
